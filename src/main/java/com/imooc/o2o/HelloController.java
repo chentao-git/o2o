909d10cc83abe6789b43ede6abc4a8e1c19302f7
company <2455581815@qq.com>
2018-08-22 10:50:36 +0800

    项目新建初始

---
+++ src/main/java/com/imooc/o2o/HelloController.java
@@ -0,0 +1,12 @@
+package com.imooc.o2o;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+    @RequestMapping("/hello")
+    public String hello(){
+        return "hello word toto !!!";
+    }
+}

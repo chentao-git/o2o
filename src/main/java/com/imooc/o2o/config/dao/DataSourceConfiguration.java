205e9220566a646929488cb47ead49016a15f8ff
company <2455581815@qq.com>
2018-08-24 19:56:07 +0800

    dao.xml

--- src/main/java/com/imooc/o2o/config/dao/DataSourceConfiguration.java
+++ src/main/java/com/imooc/o2o/config/dao/DataSourceConfiguration.java
@@ -40,5 +40,38 @@ public class DataSourceConfiguration {
     } catch (PropertyVetoException e) {
         e.printStackTrace();
     }
+    return dataSource;
 }
+
+    public String getJdbcDriver() {
+        return jdbcDriver;
+    }
+
+    public void setJdbcDriver(String jdbcDriver) {
+        this.jdbcDriver = jdbcDriver;
+    }
+
+    public String getJdbcUrl() {
+        return jdbcUrl;
+    }
+
+    public void setJdbcUrl(String jdbcUrl) {
+        this.jdbcUrl = jdbcUrl;
+    }
+
+    public String getJdbcUsername() {
+        return jdbcUsername;
+    }
+
+    public void setJdbcUsername(String jdbcUsername) {
+        this.jdbcUsername = jdbcUsername;
+    }
+
+    public String getJdbcPassword() {
+        return jdbcPassword;
+    }
+
+    public void setJdbcPassword(String jdbcPassword) {
+        this.jdbcPassword = jdbcPassword;
+    }
 }

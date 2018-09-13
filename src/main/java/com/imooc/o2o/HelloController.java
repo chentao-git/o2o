4580d7f11cfcb415e88e4a742f29b67fab3bac7a
company <2455581815@qq.com>
2018-09-13 15:01:19 +0800

    ieda springboot 热部署

--- src/main/java/com/imooc/o2o/HelloController.java
+++ src/main/java/com/imooc/o2o/HelloController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
     @RequestMapping("/hello")
     public String hello(){
-        return "hello word toto !!!";
+        return "hello word!!!";
     }
 }

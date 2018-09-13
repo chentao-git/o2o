0c1b27c3b75f71ab232c8a29394f0244d4c82dca
company <2455581815@qq.com>
2018-09-13 19:56:00 +0800

    验证码 Kaptcha 迁移

--- src/main/java/com/imooc/o2o/config/web/MvcConfiguration.java
+++ src/main/java/com/imooc/o2o/config/web/MvcConfiguration.java
@@ -1,7 +1,10 @@
 package com.imooc.o2o.config.web;
 
+import com.google.code.kaptcha.servlet.KaptchaServlet;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
@@ -13,6 +16,8 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import javax.servlet.ServletException;
 
 /**
  * 开启mvc，自动注入spring容器。WebMvcConfigurerAdapter 配置试图解析器
@@ -78,5 +83,48 @@ public class MvcConfiguration extends WebMvcConfigurerAdapter implements Applica
         multipartResolver.setMaxUploadSize(20971520);
         multipartResolver.setMaxInMemorySize(20971520);
         return multipartResolver;
+    }
+    @Value("${kaptcha.border}")
+    private String boeder;
+
+    @Value("${kaptcha.textproducer.font.color}")
+    private String fcolor;
+
+    @Value("${kaptcha.image.width}")
+    private String width;
+
+    @Value("${kaptcha.textproducer.char.string}")
+    private String cString;
+
+    @Value("${kaptcha.image.height}")
+    private String height;
+
+    @Value("${kaptcha.textproducer.font.size}")
+    private String fsize;
+
+    @Value("${kaptcha.noise.color}")
+    private String nColor;
+
+    @Value("${kaptcha.textproducer.char.length}")
+    private String clength;
+
+    @Value("${kaptcha.textproducer.font.names}")
+    private String fnames;
+    /**
+     * 由于web.xml不生效了，需要在这里配置Kaptcha验证码Servlet
+     */
+    @Bean
+    public ServletRegistrationBean servletRegistrationBean() throws ServletException{
+        ServletRegistrationBean servlet = new ServletRegistrationBean(new KaptchaServlet(),"/Kaptcha");
+        servlet.addInitParameter("kaptcha.border",boeder);//无边框
+        servlet.addInitParameter("kaptcha.textproducer.font.color",fcolor);//字体颜色
+        servlet.addInitParameter("kaptcha.image.width",width);//图片宽度
+        servlet.addInitParameter("kaptcha.textproducer.char.string",cString);//使用哪些字符生成验证码
+        servlet.addInitParameter("kaptcha.image.height",height);//图片高度
+        servlet.addInitParameter("kaptcha.textproducer.font.size",fsize);//字体大小
+        servlet.addInitParameter("kaptcha.noise.color",nColor);//干扰线的颜色
+        servlet.addInitParameter("kaptcha.textproducer.char.length",clength);//字符个数
+        servlet.addInitParameter("kaptcha.textproducer.font.names",fnames);//字体名字
+        return servlet;
     }
 }
